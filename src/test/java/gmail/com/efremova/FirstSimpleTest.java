c62d32b9ea2ba04ae8de4c2bc46c54dc89effd2e
e.efremova <e.efremova@dextechnology.com>
2021-12-16 16:39:53 +0200

    firstTest

---
+++ src/test/java/gmail/com/efremova/FirstSimpleTest.java
@@ -0,0 +1,14 @@
+package gmail.com.efremova;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Это первый простой тест")
+public class FirstSimpleTest {
+    @Test
+    void firstTest()
+    {
+     System.out.println("First test!");
+    }
+
+}
